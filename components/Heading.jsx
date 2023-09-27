0b92c0130b1c8f436356115c3273ba04c457eae2
rass <coolrass1@aol.co.uk>
2023-09-27 21:12:06 +0100

    4 commit

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import ContactButton from "./ContactButton";
 
 const Heading = () => {
   return (
-    <section className=" mb-7 md:max-w-screen-md md:mx-auto lg:max-w-screen-lg   xl:max-w-screen-xl 2xl:max-w-screen-xl ">
+    <>
+    <section className=" hidden lg:block mb-7 md:max-w-screen-md md:mx-auto lg:max-w-screen-lg   xl:max-w-screen-xl 2xl:max-w-screen-xl ">
       <main className=" pt-14 px-14 mb-7 flex justify-center items-center gap-7">
         <div className="text-sm ">
           <Image
@@ -16,13 +18,13 @@ const Heading = () => {
             objectFit="cover"
           />
         </div>
-<div className="border-2 border-red-700 translate-x-36 ">
+<div className="hidden lg:block border-2 border-red-700 translate-x-36 ">
         <button className="px-3 py-[1px] text-[10px] border-2 shadow-lg">
           Contact Us
         </button>
-        </div>
+   </div>
       </main>
-      <nav>
+      <nav className="hidden lg:block">
         <ul className="flex justify-center items-center gap-2">
           <li>Home</li>
           <li>About</li>
@@ -30,7 +32,26 @@ const Heading = () => {
           <li>Contact</li>
         </ul>
       </nav>
+      </section>
+      <section className="block lg:hidden">
+      <header className=" pt-7 flex justify-between items-center px-3 lg:hidden ">
+      <button className="relative  ">
+        <div className="w-7 h-[1px] mb-1 bg-black"></div>
+        <div className="w-6 h-[1px] mb-1 ml-1 bg-black"></div>
+        <div className="w-5 h-[1px] mb-1 ml-2 bg-black"></div>
+        <div className="w-5 h-[1px] mb-1 ml-3 bg-black"></div>
+
+      </button>
+      <ContactButton/>
+
+    </header>
+    <div className="text-center pb-3">  signature</div>
+  
     </section>
+      
+    </>
+
+
   );
 };
 
