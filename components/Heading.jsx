dd4b104d78d62cd868239e3885310b0d2943a4ab
rass <coolrass1@aol.co.uk>
2023-09-28 20:34:52 +0100

    7 commit

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -7,7 +7,7 @@ import Signature from "./Signature";
 import { TEMPORARY_REDIRECT_STATUS } from "next/dist/shared/lib/constants";
 
 const Heading = () => {
-  const [ismenu , setIsmenu]=useState(false) 
+  const [ismenu , setIsmenu]=useState(true) 
   const HandleShow=(e)=>{
     e.preventDefault()
     
@@ -48,7 +48,7 @@ const Heading = () => {
         </main>
       </section>
       <section className="block   relative lg:hidden">
-        <div className={`absolute ${ismenu && 'opacity-0'} tra transition-opacity  top-0 left-1 w-[400px] min-h-screen`}>
+        <div className={`absolute  ${ismenu && 'opacity-0'} tra transition-opacity  top-0 left-1 w-[400px] min-h-screen`}>
         <nav className="bg-white text-gray-400">
               <ul className="flex  py-7 min-h-full  flex-col justify-center items-center gap-5">
                 <li className="py-3 ">Home</li>
@@ -64,7 +64,7 @@ const Heading = () => {
             </nav>
         </div>
         <header className=" pt-7 flex justify-between items-center px-3 lg:hidden ">
-          { !ismenu?(<button onClick={HandleShow} className="relative  z-10">
+          { ismenu?(<button onClick={HandleShow} className="relative  z-10">
             <div className="w-7 h-[1px] mb-1 bg-black"></div>
             <div className="w-6 h-[1px] mb-1 ml-1 bg-black"></div>
             <div className="w-5 h-[1px] mb-1 ml-2 bg-black"></div>
